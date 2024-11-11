c66d8df1f3cb1d4866aacae87f268c1cc9734c2e
Drnyash <tugumbajhadiza@gmail.com>
2024-11-11 18:30:32 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//A
+
+let number = prompt("1 - ден 12- ге дейін сан еңгіз"); 
+
+if(number>=3 && number <= 5){
+    alert("Ai" + number + "Spring")
+}
+else if(number>=6 && number <= 8){
+    alert("Ai" + number + "Summer" )
+}
+else if(number>=9 && number <= 11){
+    alert("Ai" + number + "Autumn" )   
+}
+else if(number == 1 || number == 2 || number == 12){
+       alert("Ai" + number + "Winter" )
+}
+else{
+    alert("1 жыл 12 айдан тұрады 1 - ден 12- ге дейін сан еңгіз")
+}
+
+//B
+let a = prompt("Birinshi san engiziniz:")
+let b = prompt("Ekinshi san engiziniz:")
+
+let number12 
+
+number12 = (a + b)/2
+
+if(a > b){
+    alert("a саны b санынан үлкен")
+}
+else if(a<b){
+    alert("a саны b санынан кіші")
+}
+else{
+    alert("Сандар тең")
+}
+
+alert("Sandardin ortasha mani:" + number12);
+
+
+//C
+
+
+
+
